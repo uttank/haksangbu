956a7346c437eb3b2f1af8fb5ad3fb2f60f941fa
uttank <park.yongho@gmail.com>
2020-12-03 21:25:13 +0900

    add text_detection

--- sample.py
+++ sample.py
@@ -18,19 +18,19 @@ image = vision.Image(content=content)
 
 # Performs text detection on the image file
 response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
+texts = response.text_annotations
+print('Texts:')
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+for text in texts:
+    print('\n"{}"'.format(text.description))
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
+    vertices = (['({},{})'.format(vertex.x, vertex.y)
+               for vertex in text.bounding_poly.vertices])
 
-        print('bounds: {}'.format(','.join(vertices)))
+    print('bounds: {}'.format(','.join(vertices)))
 
-    if response.error.message:
-        raise Exception(
+if response.error.message:
+    raise Exception(
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
